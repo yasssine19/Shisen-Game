1807993166cf4f9678fde7ed5b5925c935680881
yasssine19 <167819830+yasssine19@users.noreply.github.com>
2025-02-25 17:54:17 +0100

    Create TokenState.java

---
+++ src/model/TokenState.java
@@ -0,0 +1,12 @@
+package tud.ai1.shisen.model;
+
+/**
+ * Klasse die die verschiedenen möglichen Zustaende eines Tokens festlegt.
+ * 
+ * @author  Yassine Frih
+ * @version 15.06.2023
+ */
+public enum TokenState
+{
+    DEFAULT, CLICKED, WRONG, SOLVED
+}
